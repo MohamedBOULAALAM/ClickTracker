5076955a684d9b26404ccd8da53850d121c899bb
MohamedBOULAALAM <mohamedboulaalam01@gmail.com>
2025-11-25 22:13:26 +0100

    initialize Spring Boot project with dependencies

---
+++ src/test/java/net/boulaalam/eda/EventDrivenArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package net.boulaalam.eda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
